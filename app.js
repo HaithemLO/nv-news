75c2e1da4c4c79dfd5ab51167e2f5b24857a78ac
mnemosyne <mnemosynekotorin@gmail.com>
2023-08-15 11:43:36 +0100

    nc-news-2

--- app.js
+++ app.js
@@ -10,23 +10,8 @@ const endpointsJSON = require('./endpoints.json');
 // GET /api/topics
 app.get('/api/topics',getTopics);
 
-// app.get('/api', (req, res) => {
-//     // Create a response object similar to the expected endpoints JSON
-//     const availableEndpoints = {};
-  
-//     for (const endpoint in endpointsJSON) {
-//       availableEndpoints[endpoint] = {
-//         description: endpointsJSON[endpoint].description,
-//         queries:endpointsJSON[endpoint].queries,
-//         exampleResponse:endpointsJSON[endpoint].exampleResponse,
-//         format:endpointsJSON[endpoint].format
-//       };
-//     }
-  
-//     res.status(200).json(availableEndpoints);
-//   });
 
-  app.get('/api', getApis)
+app.get('/api', getApis)
 
 app.use((err,req,res,next) => {
 
