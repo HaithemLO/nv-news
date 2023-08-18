54b4ab293dbec1e6c1ee7b9e12035608eb39f327
mnemosyne <mnemosynekotorin@gmail.com>
2023-08-18 11:17:35 +0100

    s

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+
+
 const pathToCorrectEnvFile = `${__dirname}/../.env.${ENV}`;
 
 console.log(__dirname); // /Users/username/project-folder/db
@@ -12,8 +21,8 @@ require('dotenv').config({
 
 // handle using the correct environment variables here
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
