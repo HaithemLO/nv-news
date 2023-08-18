d0c120903c88828601aecad3909a60caf70d83b4
mnemosyne <mnemosynekotorin@gmail.com>
2023-08-18 12:14:22 +0100

    s

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ app.use((err, req, res, next) => {
   
     // Handle different types of errors
     if (err.status) {
-      res.status(err.status).json({ message: err.msg });
+      res.status(err.status).send({ message: err.msg });
     }  else if (err.message === 'Invalid data type') {
-        res.status(400).json({ message: 'Invalid data type for article_id' });
+        res.status(400).send({ message: 'Invalid data type for article_id' });
       }else {
-      res.status(404).json({ message: 'Article not found' });
+      res.status(404).send({ message: 'Article not found' });
     }
   });
 
